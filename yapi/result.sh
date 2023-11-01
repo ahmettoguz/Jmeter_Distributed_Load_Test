d20291c6c140dbdc46dc57da49838640ac1d18b1
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-01 10:22:28 +0300

    .

--- yapi/result.sh
+++ yapi/result.sh
@@ -15,3 +15,10 @@ request_200_count=$(grep -o 'Request,200,' result.jtl | wc -l)
 result=$(echo "scale=2; ($total_lines - 1) / $request_200_count" | bc)
 echo -e "Pass / Total\n$request_200_count / $((total_lines - 1))" > summary.txt
 
+# display summary results
+echo
+cat summary.txt
+
+
+
+
