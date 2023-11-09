7ae1afa822ab8b72edd8ce85fcaa066a44faac34
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-09 15:19:56 +0300

    In progress.

--- Terraform/DigitalOcean/script/down.sh
+++ Terraform/DigitalOcean/script/down.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Down pods
-# kubectl delete -f ../k8s_Config/k8s.yaml
-
-# # Delete pods (if previous step is failed.)
-# kubectl delete deployments --all -n test
+kubectl delete -f ../k8s_Config/k8s.yaml
 
 # Destroy cluster with terraform
 terraform -chdir=../tf_Config destroy
