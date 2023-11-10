9c4deb12fe83be68ad179f8790bb50e520456059
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-10 15:21:18 +0300

    .

--- Terraform/AWS/k8s.tf
+++ Terraform/AWS/k8s.tf
@@ -13,17 +13,22 @@ provider "aws" {
   region     = var.region
 }
 
-module "eks" {
+module "eks_cluster" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "19.19.0"  # Kullanılabilir olan en son sürümü belirtin
   cluster_name    = var.cluster_name
   cluster_version = "1.21"
-  
-  min_size     = 1
-  max_size     = 10
-  desired_size = 1
 
-  instance_types = ["t3.micro"]
-  capacity_type  = "SPOT"
+    kube_data_auth_enabled = false
+    kube_exec_auth_enabled = true
 }
 
+
+
+module "eks_node_group" {
+  subnet_ids         = module.this.enabled ? module.subnets.public_subnet_ids : ["filler_string_for_enabled_is_false"]
+  cluster_name       = module.this.enabled ? module.eks_cluster.eks_cluster_id : "disabled"
+
+  instance_types     = "t3.micro"
+  desired_size       = 2
+}
