51d3ebabc45ef662a3faf37d57aa10d84e6aea50
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-16 11:19:29 +0300

    Terraform Azure in progress.

--- Terraform/Azure/script/token.sh
+++ Terraform/Azure/script/token.sh
@@ -13,8 +13,8 @@ app_id=$1
 app_secret=$2
 
 # Write file.
-echo 'aks_service_principal_app_id = '$app_id'
-aks_service_principal_client_secret = '$app_secret > ../tf_Config/terraform.tfvars
+echo 'aks_service_principal_app_id = '"$app_id"'
+aks_service_principal_client_secret = '"$app_secret" > ../tf_Config/terraform.tfvars
 
 # Give output.
 echo "The token variables have been set."
