47180c30a77c0d9d28b5fbcdea19970ab760c1dd
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-16 10:13:50 +0300

    Terraform Azure in progress. upTerraform.sh implemented.

---
+++ Terraform/Azure/script/upTerraform.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Aim of that script is to allocate resources. Create k8s infrastructure with terraform
+
+# Init terraform and check status
+terraform -chdir=../tf_Config init
+if [ $? -ne 0 ]; then
+    echo "Terraform init failed."
+    exit 1
+fi
+
+# Apply terraform and check status
+terraform -chdir=../tf_Config apply -auto-approve
+if [ $? -ne 0 ]; then
+    echo "Terraform apply failed."
+    exit 1
+fi
+
+echo "K8s cluster created."
