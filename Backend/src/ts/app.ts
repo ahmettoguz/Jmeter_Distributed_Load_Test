135654629a346cd0e5f242e13e2b0b57d96dfdd9
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-16 16:28:13 +0300

    Backend in progress for terraform azure.

--- Backend/src/ts/app.ts
+++ Backend/src/ts/app.ts
@@ -32,8 +32,13 @@ async function executeSh(shPath, shCommand, parameters) {
       console.error(data.toString());
     });
     proc.on("close", (code) => {
-      if (code != 0)
-        console.info(`child process close all stdio with code ${code}`);
+      if (code === 0) {
+        console.info(`child process closed with success (code ${code})`);
+        resolve({ success: true, output });
+      } else {
+        console.error(`child process closed with error (code ${code})`);
+        reject({ success: false, output });
+      }
     });
     proc.on("exit", (data) => {
       resolve(output);
@@ -94,38 +99,69 @@ async function runAzureTerraform(req) {
   // const nodeCount = req.body.nodeCount;
 
   const shPath = "../Terraform/Azure/script";
+  let result;
   let parameters;
 
-  // execute prepare sh file params: node count, pod count 
+  // execute prepare sh file params: node count, pod count
   // TODO hard coded node and pod count
   parameters = ["prepare.sh", "2", "5"];
-  await executeSh(shPath, "sh", parameters);
+  result = await executeSh(shPath, "sh", parameters);
   console.info("\nprepare.sh finished.");
+
+  if (!result.success) {
+    console.info("Process is Failed!");
+    process.exit(1);
+  }
 
   // execute upTerraform sh file
   parameters = ["upTerraform.sh"];
-  await executeSh(shPath, "sh", parameters);
+  result = await executeSh(shPath, "sh", parameters);
   console.info("\nupTerraform.sh finished.");
+
+  if (!result.success) {
+    console.info("Process is Failed!");
+    process.exit(1);
+  }
 
   // execute upCluster sh file
   parameters = ["upCluster.sh"];
   await executeSh(shPath, "sh", parameters);
   console.info("\nupCluster.sh finished.");
 
+  if (!result.success) {
+    console.info("Process is Failed!");
+    process.exit(1);
+  }
+
   // execute sh runTest sh file
   parameters = ["runTest.sh"];
   await executeSh(shPath, "sh", parameters);
   console.info("\nrunTest.sh finished.");
+
+  if (!result.success) {
+    console.info("Process is Failed!");
+    process.exit(1);
+  }
 
   // execute result sh file
   parameters = ["result.sh"];
   await executeSh(shPath, "sh", parameters);
   console.info("\nresult.sh finished.");
 
+  if (!result.success) {
+    console.info("Process is Failed!");
+    process.exit(1);
+  }
+
   // execute down sh file
   parameters = ["downTerraform.sh"];
   await executeSh(shPath, "sh", parameters);
   console.info("\ndownTerraform.sh finished.");
+
+  if (!result.success) {
+    console.info("Process is Failed!");
+    process.exit(1);
+  }
 }
 
 // ------------------------------------------------- End Points
