61ee3a00ef0b11292fde16bf0a0e2bfb53a43979
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-17 14:10:48 +0300

    Terraform Digital Ocean in progress.

---
+++ Terraform/DigitalOcean/tf_Config/k8s.tf
@@ -0,0 +1,26 @@
+variable "do_token" {}
+
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.32.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = "${var.do_token}"
+}
+
+resource "digitalocean_kubernetes_cluster" "k8sdo" {
+  name = "k8sdo"
+  region = "fra1"
+  version = "1.28.2-do.0"
+
+  node_pool {
+    name = "nodepooldo"
+    size = "s-2vcpu-2gb"
+    node_count = 1
+  }
+}
