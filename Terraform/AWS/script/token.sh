34377dc9bd684ac5ab6b1ab70fb394b3ad869b6a
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-20 08:57:00 +0300

    Terraform for Aws in progress.

--- Terraform/AWS/script/token.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Set api token of digital ocean to environment variable
-export TF_VAR_aws_access_key=$1 
-export TF_VAR_aws_secret_key=$2 
-
-# Give permissions to files
-chmod +x prepare.sh up.sh result.sh down.sh
--- Terraform/AWS/tf_config/k8s.tf
+++ Terraform/AWS/tf_config/k8s.tf
@@ -1,43 +1,48 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "region" {
-  default = "us-west-1"
-}
-variable "cluster_name" {
-  default = "k8s"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.region
+  region = "eu-north-1"
 }
 
-module "eks_cluster" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "19.19.0"  # Kullanılabilir olan en son sürümü belirtin
-  cluster_name    = var.cluster_name
-  cluster_version = "1.21"
-
-    kube_data_auth_enabled = false
-    kube_exec_auth_enabled = true
+data "aws_vpcs" "selected_vpc" {
+  ids = ["vpc-0231b5d0f3ef53955"]  # Seçilen VPC ID'si
 }
 
-
-module "eks_node_group" {
-  cluster_name       = "k8s"
-
-  instance_types     = "t3.micro"
-  desired_size       = 2
+data "aws_subnet" "selected_subnets" {
+  for_each = {
+    for idx, subnet_id in ["subnet-0eccfda288b9ffa3b", "subnet-0d934172dfd340e5a", "subnet-0545ca12b0f614088"] : idx => subnet_id
+  }
+  ids = values(each.value)
 }
 
+resource "aws_eks_cluster" "my_cluster" {
+  name     = "k8saws"
+  role_arn = aws_iam_role.eks_cluster.arn
+  vpc_config {
+    subnet_ids = [for subnet in data.aws_subnet.selected_subnets.values : subnet.id]
+  }
+}
 
-resource "aws_eks_node_group" "example" {
-  source = "./"
-  cluster_name    = "k8s"
-  node_group_name = "k8spool"
- 
-  scaling_config {
-    desired_size = 2
- }
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "eks.amazonaws.com",
+        },
+      },
+    ],
+  })
 }
