c015c1dc4309f4445b866ca0cb51b52c6a3fc6ec
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-21 14:57:27 +0300

    Terraform AWS in progress.

--- Terraform/AWS/tf_Config/k8s.tf
+++ Terraform/AWS/tf_Config/k8s.tf
@@ -7,6 +7,7 @@ variable "secret_key" {
   default = ""
 }
 
+# ############################################## 0-provider.tf
 terraform {
   required_providers {
     aws = {
@@ -21,27 +22,170 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
 }
+# ##############################################
 
-resource "aws_vpc" "k8saws_vpc" {
+
+# ############################################## 1-vpc.tf
+resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  attach_vpc_cni_policy = true
+}
+# ##############################################
+
+
+# ############################################## 2-igw.tf
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+# ############################################## 
+
+# ############################################## 3-subnets.tf
+resource "aws_subnet" "private-us-east-1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/19"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    "Name"                            = "private-us-east-1a"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
 }
 
-resource "aws_subnet" "k8saws_subnet" {
-  vpc_id                  = aws_vpc.k8saws_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-north-1a"
+resource "aws_subnet" "private-us-east-1b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.32.0/19"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    "Name"                            = "private-us-east-1b"
+    "kubernetes.io/role/internal-elb" = "1"
+    "kubernetes.io/cluster/demo"      = "owned"
+  }
 }
 
-resource "aws_subnet" "k8saws_subnet_2" {
-  vpc_id                  = aws_vpc.k8saws_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "eu-north-1b"
+resource "aws_subnet" "public-us-east-1a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.64.0/19"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                       = "public-us-east-1a"
+    "kubernetes.io/role/elb"     = "1"
+    "kubernetes.io/cluster/demo" = "owned"
+  }
 }
 
-resource "aws_security_group" "k8saws_security_group" {
-  vpc_id = aws_vpc.k8saws_vpc.id
+resource "aws_subnet" "public-us-east-1b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.96.0/19"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    "Name"                       = "public-us-east-1b"
+    "kubernetes.io/role/elb"     = "1"
+    "kubernetes.io/cluster/demo" = "owned"
+  }
 }
+# ############################################## 
+
+# ############################################## 4-nat.tf
+resource "aws_eip" "nat" {
+  vpc = true
+
+  tags = {
+    Name = "nat"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public-us-east-1a.id
+
+  tags = {
+    Name = "nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+# ##############################################
+
+# ############################################## 5-routes.tf
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route = [
+    {
+      cidr_block                 = "0.0.0.0/0"
+      nat_gateway_id             = aws_nat_gateway.nat.id
+      carrier_gateway_id         = ""
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      gateway_id                 = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
+      vpc_peering_connection_id  = ""
+    },
+  ]
+
+  tags = {
+    Name = "private"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route = [
+    {
+      cidr_block                 = "0.0.0.0/0"
+      gateway_id                 = aws_internet_gateway.igw.id
+      nat_gateway_id             = ""
+      carrier_gateway_id         = ""
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
+      vpc_peering_connection_id  = ""
+    },
+  ]
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_route_table_association" "private-us-east-1a" {
+  subnet_id      = aws_subnet.private-us-east-1a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private-us-east-1b" {
+  subnet_id      = aws_subnet.private-us-east-1b.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "public-us-east-1a" {
+  subnet_id      = aws_subnet.public-us-east-1a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-us-east-1b" {
+  subnet_id      = aws_subnet.public-us-east-1b.id
+  route_table_id = aws_route_table.public.id
+}
+# ##############################################
+
+# ############################################## 6-eks.tf
 
 resource "aws_iam_role" "k8sawsiamcluster" {
   name = "k8sawsiamcluster"
@@ -70,13 +214,21 @@ resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
 resource "aws_eks_cluster" "k8saws_cluster" {
   name     = "k8saws"
   role_arn = aws_iam_role.k8sawsiamcluster.arn
+
   vpc_config {
-    subnet_ids = [aws_subnet.k8saws_subnet.id, aws_subnet.k8saws_subnet_2.id]
+    subnet_ids = [
+      aws_subnet.private-us-east-1a.id,
+      aws_subnet.private-us-east-1b.id,
+      aws_subnet.public-us-east-1a.id,
+      aws_subnet.public-us-east-1b.id
+    ]
   }
 
  depends_on = [aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy]
 }
+# ##############################################
 
+# ############################################## 7-nodes.tf
 resource "aws_iam_role" "k8sawsiamnode" {
   name = "k8sawsiamnode"
 
@@ -113,11 +265,10 @@ resource "aws_eks_node_group" "k8sawsiamnodegroup" {
   node_role_arn   = aws_iam_role.k8sawsiamnode.arn
 
   subnet_ids = [
-    aws_subnet.k8saws_subnet.id,
-    aws_subnet.k8saws_subnet_2.id
+    aws_subnet.private-us-east-1a.id,
+    aws_subnet.private-us-east-1b.id
   ]
-
-  iam_role_attach_cni_policy = true
+  
   capacity_type  = "ON_DEMAND"
   instance_types = ["t3.micro"]
 
@@ -137,6 +288,4 @@ resource "aws_eks_node_group" "k8sawsiamnodegroup" {
     aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
-
-
-
+# ##############################################
