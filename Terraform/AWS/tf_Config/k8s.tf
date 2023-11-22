22ae16a3fe945918243a0acf5edca7392c909aa7
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-22 16:43:44 +0300

    subnets removed.

--- Terraform/AWS/tf_Config/k8s.tf
+++ Terraform/AWS/tf_Config/k8s.tf
@@ -22,94 +22,6 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
-resource "aws_vpc" "k8sawsvpc" {
-  cidr_block = "10.0.0.0/16"
-
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.k8sawsvpc.id
-}
-
-resource "aws_subnet" "k8saws_subnet" {
-  vpc_id                  = aws_vpc.k8sawsvpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "eu-north-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    "Name" = "subnet"
-    "kubernetes.io/role/elb" = "1"
-    "kubernetes.io/cluster/k8sawscluster" = "owned"
-  }
-
-  depends_on = [aws_vpc.k8sawsvpc]
-}
-
-resource "aws_subnet" "k8saws_subnet_2" {
-  vpc_id                  = aws_vpc.k8sawsvpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "eu-north-1b"
-  map_public_ip_on_launch = true
-
-   tags = {
-    "Name" = "subnet2"
-    "kubernetes.io/role/elb" = "1"
-    "kubernetes.io/cluster/k8sawscluster" = "owned"
-  }
-
-  depends_on = [aws_vpc.k8sawsvpc]
-}
-
-resource "aws_eip" "nat" {
-  vpc = true
-
-  tags = {
-    Name = "nat"
-  }
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
-  subnet_id = aws_subnet.k8saws_subnet.id
-
-  tags = {
-    Name = "nat"
-  }
-
-  depends_on = [aws_internet_gateway.igw]
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.k8sawsvpc.id
-
-  route  {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
-  }
-  
-  tags = {
-  Name = "public"
-  } 
-}
-
-resource "aws_route_table_association" "public_eu_north_1a"{
-  subnet_id = aws_subnet.k8saws_subnet.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "public_eu_north_1b"{
-  subnet_id = aws_subnet.k8saws_subnet_2.id
-  route_table_id = aws_route_table.public.id
-}
-
-
-
-
-
-
 resource "aws_iam_role" "k8sawsiamcluster" {
   name = "k8sawsiamcluster"
 
@@ -138,10 +50,10 @@ resource "aws_eks_cluster" "k8sawscluster" {
   name     = "k8saws"
   role_arn = aws_iam_role.k8sawsiamcluster.arn
   vpc_config {
-    subnet_ids = [aws_subnet.k8saws_subnet.id, aws_subnet.k8saws_subnet_2.id]
+    subnet_ids = ["subnet-0b862071f01cc12cc","subnet-04d4b4c425dc7e37c"]
   }
 
- depends_on = [aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy, aws_vpc.k8sawsvpc]
+ depends_on = [aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy]
 }
 
 resource "aws_iam_role" "k8sawsiamnode" {
@@ -158,6 +70,15 @@ resource "aws_iam_role" "k8sawsiamnode" {
     Version = "2012-10-17"
   })
 }
+
+
+
+
+
+
+
+
+
 
 resource "aws_iam_role_policy_attachment" "nodes-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
@@ -179,10 +100,7 @@ resource "aws_eks_node_group" "k8sawsiamnodegroup" {
   node_group_name = "k8sawsiamnodegroup"
   node_role_arn   = aws_iam_role.k8sawsiamnode.arn
 
-  subnet_ids = [
-    aws_subnet.k8saws_subnet.id,
-    aws_subnet.k8saws_subnet_2.id
-  ]
+  subnet_ids = ["subnet-0b862071f01cc12cc","subnet-04d4b4c425dc7e37c"]
 
   capacity_type  = "ON_DEMAND"
   instance_types = ["t3.micro"]
@@ -201,9 +119,6 @@ resource "aws_eks_node_group" "k8sawsiamnodegroup" {
     aws_iam_role_policy_attachment.nodes-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.nodes-AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
-    aws_vpc.k8sawsvpc,
-    aws_subnet.k8saws_subnet,
-    aws_subnet.k8saws_subnet_2,
   ]
 }
 
