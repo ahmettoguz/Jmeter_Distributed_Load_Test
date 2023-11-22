6d14ce6503ca05e6b45cbd20144fbe5c55022e76
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-22 12:08:01 +0300

    vpc related problem also in progress.

--- Terraform/AWS/tf_Config/k8s.tf
+++ Terraform/AWS/tf_Config/k8s.tf
@@ -24,6 +24,9 @@ provider "aws" {
 
 resource "aws_vpc" "k8saws_vpc" {
   cidr_block = "10.0.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "k8saws_subnet" {
