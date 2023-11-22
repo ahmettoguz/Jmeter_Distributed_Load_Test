c645c4c238a0a3ad307b03cc90292472123a17a2
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-22 16:24:45 +0300

    23 min for nodegroup creation still unhandeled.

--- Terraform/AWS/tf_Config/k8s.tf
+++ Terraform/AWS/tf_Config/k8s.tf
@@ -82,28 +82,17 @@ resource "aws_nat_gateway" "nat" {
   depends_on = [aws_internet_gateway.igw]
 }
 
-resource "aws_route_table" "private" {
+resource "aws_route_table" "public" {
   vpc_id = aws_vpc.k8sawsvpc.id
 
-  route = [
-    {
-      cidr_block = "0.0.0.0/0"
-      nat_gateway_id = aws_nat_gateway.nat.id
-      carrier_gateway_id = ""
-      destination_prefix_list_id = ""
-      egress_only_gateway_id = ""
-      gateway_id = ""
-      instance_id = ""
-      ipv6_cidr_block = ""
-      local_gateway_id = ""
-      network_interface_id = ""
-      transit_gateway_id = ""
-      vpc_endpoint_id = ""
-      vpc_peering_connection_id = ""
-    },
-  ]
-
-  tags = "public"
+  route  {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+  
+  tags = {
+  Name = "public"
+  } 
 }
 
 resource "aws_route_table_association" "public_eu_north_1a"{
