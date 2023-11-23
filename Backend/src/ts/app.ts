ab00e4df0f2f85cfa3a4a8bf41b199e82253023f
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-11-23 10:51:40 +0300

    Backend structure is changed according to oop.

--- Backend/src/ts/app.ts
+++ Backend/src/ts/app.ts
@@ -1,13 +1,11 @@
 const express = require("express");
-const cp = require("child_process");
 const multer = require("multer");
-const fs = require("fs");
-
 const app = express();
-const port = 80;
 
-// import HelperService from "./services/HelperService";
-// const helperService = new HelperService("Toyota", "Corolla");
+import HelperService from "./services/HelperService";
+
+const port = 80;
+const helperService = new HelperService();
 
 // -------------------------------------------------- Middleware
 app.use((req, res, next) => {
@@ -32,127 +30,6 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage });
 
 // -------------------------------------------------- Functions
-function moveJmxFile(currentPath, targetPath, res) {
-  fs.rename(currentPath, targetPath, function (err) {
-    if (err) {
-      returnNegativeMessage(res, 502, "Cannot move file!");
-      return false;
-    }
-  });
-  return true;
-}
-
-async function executeSh(shPath, shCommand, parameters) {
-  return new Promise((resolve) => {
-    let output: string[] = [];
-    const proc = cp.spawn(shCommand, parameters, {
-      cwd: shPath,
-      shell: "/bin/bash",
-      env: process.env,
-    });
-    proc.stdout.on("data", (data) => {
-      console.info(data.toString());
-      output.push(data.toString());
-    });
-    proc.stderr.on("data", (data) => {
-      console.error(data.toString());
-    });
-    proc.on("close", (code) => {
-      if (code === 0) {
-        console.info(`Child process closed with success code: ${code}`);
-        resolve({ success: true, output });
-      } else {
-        console.error(`Child process closed with error code: ${code}`);
-        resolve({ success: false, output });
-      }
-    });
-    proc.on("exit", (code) => {
-      if (code !== 0) {
-        console.error(`child process exited with non-zero code: ${code}`);
-        resolve({ success: false, output });
-      }
-    });
-  });
-}
-
-async function returnPositiveMessage(res, message, data) {
-  const jsResult = {
-    status: 200,
-    state: true,
-    message: message,
-  };
-  if (data != null) jsResult["data"] = data;
-  res.status(200).json(jsResult);
-}
-
-async function returnNegativeMessage(res, statusCode, errorMessage) {
-  const jsResult = {
-    status: statusCode,
-    state: false,
-    errorMessage: errorMessage,
-  };
-  res.status(statusCode).json(jsResult);
-}
-
-async function calculateResources(
-  cloudProvider,
-  virtualUser,
-  threadCountPerPod
-) {
-  let podPerNode;
-
-  const plannedPodCount = Math.ceil(virtualUser / threadCountPerPod);
-
-  // Your logic to calculate the values
-  if (cloudProvider == "AWS") podPerNode = 1;
-  else podPerNode = 5;
-
-  const plannedNodeCount = Math.ceil(plannedPodCount / podPerNode);
-
-  // Return an object with properties
-  return { podPerNode, plannedPodCount, plannedNodeCount };
-}
-
-async function checkNodeCount(cloudProvider, plannedNodeCount, res) {
-  let message;
-  let status = true;
-
-  if (cloudProvider == "Azure" && plannedNodeCount > 2) {
-    message = `Azure could provider, do not offer more than 4 cpu in free tier. Current: ${
-      plannedNodeCount * 2
-    }`;
-    status = false;
-  } else if (cloudProvider == "DigitalOcean" && plannedNodeCount > 8) {
-    message = `Digital Ocean could provider, do not offer more than 8 node in 1 node group with free tier. Current: ${plannedNodeCount}`;
-    status = false;
-  }
-
-  if (status) return true;
-  else {
-    returnNegativeMessage(res, 502, message);
-    return false;
-  }
-}
-
-async function checkCloudProvider(cloudProvider, res) {
-  if (
-    cloudProvider != "DigitalOcean" &&
-    cloudProvider != "Azure" &&
-    cloudProvider != "AWS"
-  ) {
-    returnNegativeMessage(res, 400, "Cloud provider is invalid!");
-    return false;
-  }
-  return true;
-}
-
-async function checkFile(uploadedFile, res) {
-  if (uploadedFile == undefined) {
-    returnNegativeMessage(res, 400, "Jmx file is not provided!");
-    return false;
-  }
-  return true;
-}
 
 async function runAllSteps(
   req,
@@ -176,7 +53,7 @@ async function runAllSteps(
       threadCountPerPod,
       duration,
     ];
-    result = await executeSh(shPath, "sh", parameters);
+    result = await helperService.executeSh(shPath, "sh", parameters);
     console.info("\nprepare.sh finished.");
 
     if (!result.success) {
@@ -186,7 +63,7 @@ async function runAllSteps(
 
     // execute upTerraform sh file
     parameters = ["upTerraform.sh"];
-    result = await executeSh(shPath, "sh", parameters);
+    result = await helperService.executeSh(shPath, "sh", parameters);
     console.info("\nupTerraform.sh finished.");
 
     if (!result.success) {
@@ -196,7 +73,7 @@ async function runAllSteps(
 
     // execute upCluster sh file
     parameters = ["upCluster.sh"];
-    result = await executeSh(shPath, "sh", parameters);
+    result = await helperService.executeSh(shPath, "sh", parameters);
     console.info("\nupCluster.sh finished.");
 
     if (!result.success) {
@@ -206,7 +83,7 @@ async function runAllSteps(
 
     // execute sh runTest sh file
     parameters = ["runTest.sh"];
-    result = await executeSh(shPath, "sh", parameters);
+    result = await helperService.executeSh(shPath, "sh", parameters);
     console.info("\nrunTest.sh finished.");
 
     if (!result.success) {
@@ -216,7 +93,7 @@ async function runAllSteps(
 
     // execute result sh file
     parameters = ["result.sh"];
-    result = await executeSh(shPath, "sh", parameters);
+    result = await helperService.executeSh(shPath, "sh", parameters);
     console.info("\nresult.sh finished.");
 
     if (!result.success) {
@@ -226,7 +103,7 @@ async function runAllSteps(
 
     // execute down sh file
     parameters = ["downTerraform.sh"];
-    result = await executeSh(shPath, "sh", parameters);
+    result = await helperService.executeSh(shPath, "sh", parameters);
     console.info("\ndownTerraform.sh finished.");
 
     if (!result.success) {
@@ -238,7 +115,7 @@ async function runAllSteps(
 
     // down terraform if there was an error
     parameters = ["downTerraform.sh"];
-    result = await executeSh(shPath, "sh", parameters);
+    result = await helperService.executeSh(shPath, "sh", parameters);
     console.info("\ndownTerraform.sh finished.");
   }
 }
@@ -249,7 +126,7 @@ app.get("/", async (req, res) => {
     `---\nIncoming request to: ${req.url}\nMethod: ${req.method}\nIp: ${req.connection.remoteAddress}\n---\n`
   );
 
-  return returnPositiveMessage(res, "Service is up", null);
+  return helperService.returnPositiveMessage(res, "Service is up", null);
 });
 
 app.post("/runTest", upload.single("jmxFile"), async (req, res) => {
@@ -266,21 +143,32 @@ app.post("/runTest", upload.single("jmxFile"), async (req, res) => {
 
   // get pod and node count according to could provider
   const { podPerNode, plannedPodCount, plannedNodeCount } =
-    await calculateResources(cloudProvider, virtualUser, threadCountPerPod);
+    await helperService.calculateResources(
+      cloudProvider,
+      virtualUser,
+      threadCountPerPod
+    );
 
   // check free tier node counts
-  if ((await checkNodeCount(cloudProvider, plannedNodeCount, res)) === false)
+  if (
+    (await helperService.checkNodeCount(
+      cloudProvider,
+      plannedNodeCount,
+      res
+    )) === false
+  )
     return;
 
   // check cloud provider
-  if ((await checkCloudProvider(cloudProvider, res)) === false) return;
+  if ((await helperService.checkCloudProvider(cloudProvider, res)) === false)
+    return;
 
   // check file
-  if ((await checkFile(uploadedFile, res)) === false) return;
+  if ((await helperService.checkFile(uploadedFile, res)) === false) return;
 
   // place jmx file to related path
   if (
-    (await moveJmxFile(
+    (await helperService.moveJmxFile(
       "./upload/loadtest.jmx",
       `../Terraform/${cloudProvider}/jmx_Config/loadtest.jmx`,
       res
@@ -298,7 +186,7 @@ app.post("/runTest", upload.single("jmxFile"), async (req, res) => {
   //   duration
   // );
 
-  return returnPositiveMessage(res, "Operations started.", [
+  return helperService.returnPositiveMessage(res, "Operations started.", [
     `Planned node count : ${plannedNodeCount}`,
     `Planned pod count : ${plannedPodCount}`,
     `Thread count for each pod: ${threadCountPerPod}`,
@@ -308,7 +196,7 @@ app.post("/runTest", upload.single("jmxFile"), async (req, res) => {
 
 // ----------------------------------------------------------- Temporary endpoint for frontend trials
 app.post("/temp", upload.single("jmxFile"), (req, res) => {
-  return returnPositiveMessage(res, "temp message", null);
+  return helperService.returnPositiveMessage(res, "temp message", null);
 });
 
 app.listen(port, () => {
--- Backend/src/ts/services/HelperService.ts
+++ Backend/src/ts/services/HelperService.ts
@@ -1,30 +1,123 @@
+const cp = require("child_process");
+const fs = require("fs");
+
 class HelperService {
-  private marka: string;
-  private model: string;
-  private hiz: number;
-
-  constructor(marka: string, model: string) {
-    this.marka = marka;
-    this.model = model;
-    this.hiz = 10;
+  moveJmxFile(currentPath, targetPath, res) {
+    fs.rename(currentPath, targetPath, (err) => {
+      if (err) {
+        this.returnNegativeMessage(res, 502, "Cannot move file!");
+        return false;
+      }
+    });
+    return true;
   }
 
-  hiziGoster(): void {
-    console.log(`${this.marka} ${this.model}'in hızı: ${this.hiz} km/s`);
+  async executeSh(shPath, shCommand, parameters) {
+    return new Promise((resolve) => {
+      let output: string[] = [];
+      const proc = cp.spawn(shCommand, parameters, {
+        cwd: shPath,
+        shell: "/bin/bash",
+        env: process.env,
+      });
+      proc.stdout.on("data", (data) => {
+        console.info(data.toString());
+        output.push(data.toString());
+      });
+      proc.stderr.on("data", (data) => {
+        console.error(data.toString());
+      });
+      proc.on("close", (code) => {
+        if (code === 0) {
+          console.info(`Child process closed with success code: ${code}`);
+          resolve({ success: true, output });
+        } else {
+          console.error(`Child process closed with error code: ${code}`);
+          resolve({ success: false, output });
+        }
+      });
+      proc.on("exit", (code) => {
+        if (code !== 0) {
+          console.error(`child process exited with non-zero code: ${code}`);
+          resolve({ success: false, output });
+        }
+      });
+    });
   }
 
-  hizlan(kacKm: number): void {
-    this.hiz += kacKm;
-    console.log(
-      `${this.marka} ${this.model} hızlandı! Yeni hız: ${this.hiz} km/s`
-    );
+  async returnPositiveMessage(res, message, data) {
+    const jsResult = {
+      status: 200,
+      state: true,
+      message: message,
+    };
+    if (data != null) jsResult["data"] = data;
+    res.status(200).json(jsResult);
   }
 
-  yavasla(kacKm: number): void {
-    this.hiz -= kacKm;
-    console.log(
-      `${this.marka} ${this.model} yavaşladı! Yeni hız: ${this.hiz} km/s`
-    );
+  async returnNegativeMessage(res, statusCode, errorMessage) {
+    const jsResult = {
+      status: statusCode,
+      state: false,
+      errorMessage: errorMessage,
+    };
+    res.status(statusCode).json(jsResult);
+  }
+
+  async calculateResources(cloudProvider, virtualUser, threadCountPerPod) {
+    let podPerNode;
+
+    const plannedPodCount = Math.ceil(virtualUser / threadCountPerPod);
+
+    // Your logic to calculate the values
+    if (cloudProvider == "AWS") podPerNode = 1;
+    else podPerNode = 5;
+
+    const plannedNodeCount = Math.ceil(plannedPodCount / podPerNode);
+
+    // Return an object with properties
+    return { podPerNode, plannedPodCount, plannedNodeCount };
+  }
+
+  async checkNodeCount(cloudProvider, plannedNodeCount, res) {
+    let message;
+    let status = true;
+
+    if (cloudProvider == "Azure" && plannedNodeCount > 2) {
+      message = `Azure could provider, do not offer more than 4 cpu in free tier. Current: ${
+        plannedNodeCount * 2
+      }`;
+      status = false;
+    } else if (cloudProvider == "DigitalOcean" && plannedNodeCount > 8) {
+      message = `Digital Ocean could provider, do not offer more than 8 node in 1 node group with free tier. Current: ${plannedNodeCount}`;
+      status = false;
+    }
+
+    if (status) return true;
+    else {
+      this.returnNegativeMessage(res, 502, message);
+      return false;
+    }
+  }
+
+  async checkCloudProvider(cloudProvider, res) {
+    if (
+      cloudProvider != "DigitalOcean" &&
+      cloudProvider != "Azure" &&
+      cloudProvider != "AWS"
+    ) {
+      this.returnNegativeMessage(res, 400, "Cloud provider is invalid!");
+      return false;
+    }
+    return true;
+  }
+
+  async checkFile(uploadedFile, res) {
+    if (uploadedFile == undefined) {
+      this.returnNegativeMessage(res, 400, "Jmx file is not provided!");
+      return false;
+    }
+    return true;
   }
 }
 
