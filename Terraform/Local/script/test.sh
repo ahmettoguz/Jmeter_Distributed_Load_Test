84be79ee517f195f5a592229e35f8b13fcab9c48
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-12-04 09:25:40 +0300

    Output sytle is changed for local and will be test.

--- Terraform/Local/script/test.sh
+++ Terraform/Local/script/test.sh
@@ -12,9 +12,9 @@ IFS=" " read -ra slave_ips <<< "$ips"
 ips=$(IFS=,; echo "${slave_ips[*]}")
 
 # Remove previous results
-if [ -e "result.jtl" ]; then
-    rm result.jtl
+if [ -d "./result" ]; then
+  rm -rf "./result"
 fi
 
 # Run jmeter test
-jmeter -Jserver.rmi.ssl.disable=true -n -t loadtest.jmx -R $ips -l ./result.jtl
+jmeter -Jserver.rmi.ssl.disable=true -n -t loadtest.jmx -R $ips -l ./result/results.jtl -j ./result/jmeter.log -e -o ./result/report
