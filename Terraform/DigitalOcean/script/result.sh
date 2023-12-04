5751b7ef760967702ccac98f7dab9364e3c49ae9
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-12-04 16:29:42 +0300

    digital ocean report generator rollback

--- Terraform/DigitalOcean/script/result.sh
+++ Terraform/DigitalOcean/script/result.sh
@@ -11,36 +11,21 @@ masterName=$(kubectl get pods -n test -l jmeter_mode=master -o=jsonpath='{.items
 # Create result folder
 mkdir -p ../results/test_${timestamp}
 
-# # variable for target directory
-# resultDir="../results/test_${timestamp}"
-
-# # Copy results
-# kubectl cp -n test $masterName:/jmeter/apache-jmeter-5.1/bin/result.jtl $resultDir/result.jtl
-# kubectl cp -n test $masterName:/jmeter/apache-jmeter-5.1/bin/jmeter.log $resultDir/jmeter.log
-
-# # Write summary file
-# echo "Results saved to: $resultDir"
-
-# # Write jmeter.log summary
-# grep 'summary =' $resultDir/jmeter.log > $resultDir/summary.txt
-
-# # Display summary results
-# echo "$(cat $resultDir/summary.txt)"
-
 # variable for target directory
 resultDir="../results/test_${timestamp}"
 
 # Copy results
-kubectl cp -n test $masterName:/jmeter/apache-jmeter-5.1/bin/result $resultDir/result
+kubectl cp -n test $masterName:/jmeter/apache-jmeter-5.1/bin/result.jtl $resultDir/result.jtl
+kubectl cp -n test $masterName:/jmeter/apache-jmeter-5.1/bin/jmeter.log $resultDir/jmeter.log
 
 # Write summary file
 echo "Results saved to: $resultDir"
 
 # Write jmeter.log summary
-grep 'summary =' $resultDir/result/jmeter.log | sed 's/^[^=]*=//g' | sed '/^[[:space:]]*[0-9]/!b;n;c\' > $resultDir/result/summary.txt
+grep 'summary =' $resultDir/jmeter.log > $resultDir/summary.txt
 
 # Display summary results
-echo "$(cat $resultDir/result/summary.txt)"
+echo "$(cat $resultDir/summary.txt)"
 
 echo "Results saved."
 echo "Success"
--- Terraform/DigitalOcean/script/test.sh
+++ Terraform/DigitalOcean/script/test.sh
@@ -11,21 +11,10 @@ IFS=" " read -ra slave_ips <<< "$ips"
 # Ip related
 ips=$(IFS=,; echo "${slave_ips[*]}")
 
-# # Remove previous results
-# if [ -e "result.jtl" ]; then
-#     rm result.jtl
-# fi
-
-# # Run jmeter test
-# jmeter -Jserver.rmi.ssl.disable=true -n -t loadtest.jmx -R $ips -l ./result.jtl
-
 # Remove previous results
-if [ -d "./result" ]; then
-  rm -rf "./result"
+if [ -e "result.jtl" ]; then
+    rm result.jtl
 fi
 
-# Create result folders
-mkdir -p ./result/report
-
 # Run jmeter test
-jmeter -Jserver.rmi.ssl.disable=true -n -t loadtest.jmx -R $ips -l ./result/results.jtl -j ./result/jmeter.log -e -o ./result/report
+jmeter -Jserver.rmi.ssl.disable=true -n -t loadtest.jmx -R $ips -l ./result.jtl
