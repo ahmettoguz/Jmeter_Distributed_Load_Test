1db77a01fad0d3d219fe0780aaeb94b7e5229432
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-12-05 10:46:27 +0300

    results changed as generating report

--- Terraform/Local/script/result.sh
+++ Terraform/Local/script/result.sh
@@ -2,6 +2,18 @@
 
 # Aim of that script is to bring results to local
 
+# # Check parameters.
+# if [ $# -ne 4 ]; then
+#   echo "Invalid parameters! use following."
+#   echo "$0 <node count> <pod count> <thread count> <duration>"
+#   exit 1
+# fi
+
+# node=$1
+# pod=$2
+# thread=$3
+# duration=$4
+
 # get time stamp
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 
@@ -15,8 +27,7 @@ mkdir -p ../results/test_${timestamp}
 resultDir="../results/test_${timestamp}"
 
 # Copy results
-kubectl cp -n test $masterName:/jmeter/apache-jmeter-5.1/bin/result.jtl $resultDir/result.jtl
-kubectl cp -n test $masterName:/jmeter/apache-jmeter-5.1/bin/jmeter.log $resultDir/jmeter.log
+kubectl cp -n test $masterName:/jmeter/apache-jmeter-5.1/bin/result $resultDir
 
 # Write summary file
 echo "Results saved to: $resultDir"
