b2ee6aaac25dc245ceada1d69fb6465199f20ae3
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-12-10 13:45:36 +0300

    backend refactor

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import runApp from './app/express';
-import connectDB from './db/connection/connectDB';
+import connectDB from './db/connection';
 
 dotenv.config();
 connectDB();
