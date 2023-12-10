b631dc7a65bb78b1acd6a7238bf809643f4a67a4
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-12-11 01:06:32 +0300

    middleware in progress.

--- Backend/src/endpoint/crud/userInfo.ts
+++ Backend/src/endpoint/crud/userInfo.ts
@@ -5,8 +5,8 @@ import database from "../../database/crud/";
 
 const userInfo = async (req, res) => {
   console.log("info içerisinde");
-  const user = await database.read.getUser(1);
   return helperService.returnResponse(res, 200, true, "USSER.");
 };
+// const user = await database.read.getUser(1);
 
 export default userInfo;
