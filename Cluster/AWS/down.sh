0b2818b020e764866c3d50c7bf55f0fc8fad52c8
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-12-10 14:38:46 +0300

    refactor

--- Cluster/AWS/down.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Down pods
-kubectl delete -f k8.yaml
-
-# # Delete pods (if previous step is failed.)
-# kubectl delete deployments --all -n test
--- Cluster/DigitalOcean/script/down.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Down pods
-kubectl delete -f ../k8s_Config/k8s.yaml
-
-# # Delete pods (if previous step is failed.)
-# kubectl delete deployments --all -n test
