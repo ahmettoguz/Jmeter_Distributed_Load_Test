820a4378fda94cd5da2d5c8a168e5a9b3c2550cc
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-12-11 02:12:32 +0300

    userInfo in progress.

--- Backend/src/database/crud/read/Read.ts
+++ Backend/src/database/crud/read/Read.ts
@@ -6,9 +6,8 @@ const jwt = require("jsonwebtoken");
 class Read {
   async getUser(userId) {
     try {
-      const user: any = (await model.User.findOne({ _id: userId })).populate("tier");
-      // const user: any = await model.User.findOne({ _id: userId }).populate("Tier");
-      // const user: any = await model.User.findOne({ _id: userId }).populate("tier").populate("test");
+      // const user: any = (await model.User.findOne({ _id: userId })).populate("tier");
+      const user: any = await model.User.findOne({ _id: userId }).populate("tier").populate("test");
 
       // remove password
       // delete user.data.password;
