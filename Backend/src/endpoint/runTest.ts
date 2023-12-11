97f6ccd3a0f55eb323aa9efdaf1ae9ee60fd388f
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-12-11 08:09:55 +0300

    create test in progress.

--- Backend/src/endpoint/runTest.ts
+++ Backend/src/endpoint/runTest.ts
@@ -1,45 +1,95 @@
-import HelperService from '../services/HelperService';
+import HelperService from "../services/HelperService";
+import authService from "../services/AuthService";
+import database from "../database/crud/index";
 
 const runTest = async (req, res) => {
-    console.info(`---\nIncoming request to: ${req.url}\nMethod: ${req.method}\nIp: ${req.connection.remoteAddress}\n---\n`);
-    const cloudProvider = req.body.cloudProvider;
-    const virtualUser = req.body.virtualUser;
-    const uploadedFile = req.file;
-    const duration = 240;
-    const threadCountPerPod = 50;
+  console.info(
+    `---\nIncoming request to: ${req.url}\nMethod: ${req.method}\nIp: ${req.connection.remoteAddress}\n---\n`
+  );
+  const cloudProvider = req.body.cloudProvider;
+  const virtualUser = req.body.virtualUser;
+  const testName = req.body.testName;
+  const uploadedFile = req.file;
+  const duration = 240;
+  const threadCountPerPod = 50;
 
-    // check cloud provider
-    const checkCloudProviderRes = await HelperService.checkCloudProvider(cloudProvider);
-    if (!checkCloudProviderRes.status)
-        return HelperService.returnResponse(res, 400, false, checkCloudProviderRes.message);
+  // check cloud provider
+  const checkCloudProviderRes = await HelperService.checkCloudProvider(
+    cloudProvider
+  );
+  if (!checkCloudProviderRes.status)
+    return HelperService.returnResponse(
+      res,
+      400,
+      false,
+      checkCloudProviderRes.message
+    );
 
-    // check file
-    const checkFileRes = await HelperService.checkFile(uploadedFile);
-    if (!checkFileRes.status)
-        return HelperService.returnResponse(res, 400, false, checkFileRes.message);
+  // check file
+  const checkFileRes = await HelperService.checkFile(uploadedFile);
+  if (!checkFileRes.status)
+    return HelperService.returnResponse(res, 400, false, checkFileRes.message);
 
-    // place jmx file to related path
-    const moveJmxFileRes = await HelperService.moveJmxFile('./src/storage/upload/loadtest.jmx', `./src/Terraform/${cloudProvider}/jmx_Config/loadtest.jmx`);
-    if (!moveJmxFileRes.status)
-        return HelperService.returnResponse(res, 502, false, moveJmxFileRes.message);
+  // place jmx file to related path
+  const moveJmxFileRes = await HelperService.moveJmxFile(
+    "./src/storage/upload/loadtest.jmx",
+    `./src/Terraform/${cloudProvider}/jmx_Config/loadtest.jmx`
+  );
+  if (!moveJmxFileRes.status)
+    return HelperService.returnResponse(
+      res,
+      502,
+      false,
+      moveJmxFileRes.message
+    );
 
-    // get pod and node count according to could provider
-    const { plannedPodCount, plannedNodeCount } = await HelperService.calculateResources(cloudProvider, virtualUser, threadCountPerPod);
+  // get pod and node count according to could provider
+  const { plannedPodCount, plannedNodeCount } =
+    await HelperService.calculateResources(
+      cloudProvider,
+      virtualUser,
+      threadCountPerPod
+    );
 
-    // check free tier node counts
-    const checkNodeCountRes = await HelperService.checkNodeCount(cloudProvider, plannedNodeCount);
-    if (!checkNodeCountRes.status)
-        return HelperService.returnResponse(res, 502, false, checkNodeCountRes.message);
+  // check free tier node counts
+  const checkNodeCountRes = await HelperService.checkNodeCount(
+    cloudProvider,
+    plannedNodeCount
+  );
+  if (!checkNodeCountRes.status)
+    return HelperService.returnResponse(
+      res,
+      502,
+      false,
+      checkNodeCountRes.message
+    );
 
-    // start sh operations
-    HelperService.runAllSteps(`./src/Terraform/${cloudProvider}/script`, plannedNodeCount, plannedPodCount, threadCountPerPod, duration);
+  // get user id to insert test
+  const userId = await authService.getUserIdFromJwt();
 
-    return HelperService.returnResponse(res, 200, true, 'Operations started.', [
-        `Planned node count : ${plannedNodeCount}`,
-        `Planned pod count : ${plannedPodCount}`,
-        `Thread count for each pod: ${threadCountPerPod}`,
-        `Cloud Provider : ${cloudProvider}`,
-    ]);
+  // insert test to database
+  const insertedTestId = await database.create.insertTest(
+    userId,
+    virtualUser,
+    testName
+  );
+
+  // start sh operations
+  HelperService.runAllSteps(
+    `./src/Terraform/${cloudProvider}/script`,
+    plannedNodeCount,
+    plannedPodCount,
+    threadCountPerPod,
+    duration,
+    insertedTestId
+  );
+
+  return HelperService.returnResponse(res, 200, true, "Operations started.", [
+    `Planned node count : ${plannedNodeCount}`,
+    `Planned pod count : ${plannedPodCount}`,
+    `Thread count for each pod: ${threadCountPerPod}`,
+    `Cloud Provider : ${cloudProvider}`,
+  ]);
 };
 
 export default runTest;
