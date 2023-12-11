dabf5f032f3a9e5360cd96e02e5fb6584156c798
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2023-12-11 16:42:46 +0300

    userUpdate in progress.

--- Backend/src/route/index.ts
+++ Backend/src/route/index.ts
@@ -10,7 +10,7 @@ const userRouter: Router = Router();
 userRouter.route('/userInfo').get(helperService.displayRequestInfo, authService.isJwtValid, route.userInfo);
 userRouter.route('/login').post(helperService.displayRequestInfo, route.login);
 userRouter.route('/signUp').post(helperService.displayRequestInfo, route.signUp);
-userRouter.route('/updateUser').post(helperService.displayRequestInfo, route.userUpdate);
+userRouter.route('/userUpdate').post(helperService.displayRequestInfo, route.userUpdate);
 
 // run test
 userRouter.route('/runTest').post(helperService.displayRequestInfo, multer.single('jmxFile'),authService.isJwtValid, route.runTest);
